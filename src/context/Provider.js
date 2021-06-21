93aa61f295391adcee5a06837a3bb46200e406c0
Iago de Paula <iago.pferreiravr@gmail.com>
2021-06-21 19:16:41 -0300

    exercise-one

---
+++ src/context/Provider.js
@@ -0,0 +1,49 @@
+import PropTypes from 'prop-types';
+import React, { Component } from 'react';
+import MyContext from './MyContext';
+
+class Provider extends Component {
+  constructor(props) {
+    super(props)
+
+    this.state = {
+      cars: {
+        red: false,
+        blue: false,
+        yellow: false,
+      }
+    }
+
+    this.moveCar = this.moveCar.bind(this);
+  }
+
+  moveCar(car, side) {
+    this.setState({
+      cars: {
+        ...this.setState,
+        [car]: side,
+      }
+    })
+  }
+
+  render() {
+    const context = {
+      ...this.state,
+      moveCar: this.moveCar,
+    }
+
+    const { children } = this.props;
+
+    return (
+      <MyContext.Provider value={context}>
+        {children}
+      </MyContext.Provider>
+    )
+  }
+}
+
+Provider.propTypes = {
+  children: PropTypes.node,
+}.isRequired;
+
+export default Provider;
